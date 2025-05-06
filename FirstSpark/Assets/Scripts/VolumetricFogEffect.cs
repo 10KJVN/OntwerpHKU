e26c5bcba8c671b8e1548f0b7c6e53b94985bc1e
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-05-06 04:39:14 +0200

    Added override & enabled compability mode.
    Trouble with deprecated methods of the ScriptableRenderPass API, still needs proper conversion to RenderGraphAPI which wouldn't work. Attempt to get it working with the deprecated methods though

--- FirstSpark/Assets/Scripts/VolumetricFogEffect.cs
+++ FirstSpark/Assets/Scripts/VolumetricFogEffect.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
-public class VolumetricFogEffect : MonoBehaviour
+[System.Serializable, VolumeComponentMenu("Custom/VolumetricFogEffect")]
+public class VolumetricFogEffect : VolumeComponent, IPostProcessComponent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public ClampedFloatParameter stepSize = new ClampedFloatParameter(1f, 0.1f, 20f);
+    public FloatParameter maxDistance = new FloatParameter(100f);
+    public ColorParameter fogColor = new ColorParameter(Color.white, true);
+    public FloatParameter densityMultiplier = new ClampedFloatParameter(1f, 0f, 10f);
+    public FloatParameter noiseOffset = new FloatParameter(0f);
+    public TextureParameter fogNoise = new TextureParameter(null);
+    public FloatParameter noiseTiling = new FloatParameter(1f);
+    public FloatParameter densityThreshold = new ClampedFloatParameter(0.1f, 0f, 1f);
+    public ColorParameter lightContribution = new ColorParameter(Color.white, true);
+    public FloatParameter lightScattering = new ClampedFloatParameter(0.2f, 0f, 1f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool IsActive() => active && densityMultiplier.value > 0f;
+    public bool IsTileCompatible() => false;
 }
--- FirstSpark/Assets/Scripts/VolumetricFogFeature.cs
+++ FirstSpark/Assets/Scripts/VolumetricFogFeature.cs
@@ -1,16 +1,99 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
 
-public class VolumetricFogFeature : MonoBehaviour
+public class VolumetricFogFeature : ScriptableRendererFeature
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    class VolumetricFogPass : ScriptableRenderPass
     {
+        private Material fogMaterial;
+
+        public VolumetricFogPass(Material material)
+        {
+            fogMaterial = material;
+            renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        }
+
+        /*public void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+            TextureHandle colorTexture = resourceData.activeColorTexture;
+
+            renderGraph.AddRenderPass<PassData>("Volumetric Fog", (ref PassData data, RenderGraphContext ctx) =>
+            {
+                var stack = VolumeManager.instance.stack;
+                var volume = stack.GetComponent<VolumetricFogEffect>();
+                if (volume == null || !volume.IsActive()) return;
+
+                // Set shader properties
+                fogMaterial.SetFloat("_StepSize", volume.stepSize.value);
+                fogMaterial.SetFloat("_MaxDistance", volume.maxDistance.value);
+                fogMaterial.SetColor("_Color", volume.fogColor.value);
+                fogMaterial.SetFloat("_DensityMultiplier", volume.densityMultiplier.value);
+                fogMaterial.SetFloat("_NoiseOffset", volume.noiseOffset.value);
+                fogMaterial.SetTexture("_FogNoise", volume.fogNoise.value);
+                fogMaterial.SetFloat("_NoiseTiling", volume.noiseTiling.value);
+                fogMaterial.SetFloat("_DensityThreshold", volume.densityThreshold.value);
+                fogMaterial.SetColor("_LightContribution", volume.lightContribution.value);
+                fogMaterial.SetFloat("_LightScattering", volume.lightScattering.value);
+
+                CoreUtils.SetRenderTarget(ctx.cmd, colorTexture, ClearFlag.None);
+                ctx.cmd.Blit(colorTexture, colorTexture, fogMaterial, 0);
+            });
+        }*/
+
+        //private class PassData { }
         
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            var stack = VolumeManager.instance.stack;
+            var volume = stack.GetComponent<VolumetricFogEffect>();
+            if (volume == null || !volume.IsActive()) return;
+
+            // Set shader properties
+            fogMaterial.SetFloat("_StepSize", volume.stepSize.value);
+            fogMaterial.SetFloat("_MaxDistance", volume.maxDistance.value);
+            fogMaterial.SetColor("_Color", volume.fogColor.value);
+            fogMaterial.SetFloat("_DensityMultiplier", volume.densityMultiplier.value);
+            fogMaterial.SetFloat("_NoiseOffset", volume.noiseOffset.value);
+            fogMaterial.SetTexture("_FogNoise", volume.fogNoise.value);
+            fogMaterial.SetFloat("_NoiseTiling", volume.noiseTiling.value);
+            fogMaterial.SetFloat("_DensityThreshold", volume.densityThreshold.value);
+            fogMaterial.SetColor("_LightContribution", volume.lightContribution.value);
+            fogMaterial.SetFloat("_LightScattering", volume.lightScattering.value);
+
+            // Get the camera color target and perform a Blit
+            var cameraColor = renderingData.cameraData.renderer.cameraColorTargetHandle;
+            CommandBuffer cmd = CommandBufferPool.Get("VolumetricFogPass");
+            cmd.SetRenderTarget(cameraColor);
+            cmd.Blit(cameraColor, cameraColor, fogMaterial);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Shader fogShader;
+    private Material fogMaterial;
+    private VolumetricFogPass fogPass;
+
+    public override void Create()
     {
-        
+        if (fogShader == null)
+            fogShader = Shader.Find("Custom/Shader01");
+
+        if (fogShader != null)
+        {
+            fogMaterial = CoreUtils.CreateEngineMaterial(fogShader);
+            fogPass = new VolumetricFogPass(fogMaterial);
+        }
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        //renderer.EnqueueRenderPass(fogPass);
+        renderer.EnqueuePass(fogPass);
     }
 }
