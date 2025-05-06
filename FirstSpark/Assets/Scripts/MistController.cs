f9bb14f1d95e2a074f06ae94d555cdda103b3959
10KJVN <90614291+10KJVN@users.noreply.github.com>
2025-05-06 16:47:06 +0200

    Added reset function and fixed values

--- FirstSpark/Assets/Scripts/MistController.cs
+++ FirstSpark/Assets/Scripts/MistController.cs
@@ -8,9 +8,15 @@ public class MistController : MonoBehaviour
     private float duration = 3f;
     private bool fading = false;
 
+    private void Start()
+    {
+        ResetMistSettings(); // Reset mist settings bij levelstart
+    }
+
     public void StartFade()
     {
         fading = true;
+        timer = 0f;
     }
 
     void Update()
@@ -20,11 +26,21 @@ public class MistController : MonoBehaviour
         timer += Time.deltaTime;
         float t = Mathf.Clamp01(timer / duration);
 
-        mistMaterial.SetFloat("_DensityMultiplier", Mathf.Lerp(1f, 0.5f, t));
-        mistMaterial.SetFloat("_DensityThreshold", Mathf.Lerp(0.2f, 0.9f, t));
-        mistMaterial.SetFloat("_NoiseTiling", Mathf.Lerp(1f, 5f, t));
-        mistMaterial.SetFloat("_StepSize", Mathf.Lerp(1f, 2f, t));
-        mistMaterial.SetFloat("_MaxDistance", Mathf.Lerp(50f, 100f, t));
-        mistMaterial.SetFloat("_LightScattering", Mathf.Lerp(0.3f, 1f, t));
+        mistMaterial.SetFloat("_DensityMultiplier", Mathf.Lerp(0.15f, 0.5f, t));
+        mistMaterial.SetFloat("_DensityThreshold", Mathf.Lerp(0.276f, 0.9f, t));
+        mistMaterial.SetFloat("_NoiseTiling", Mathf.Lerp(0.5f, 5f, t));
+        mistMaterial.SetFloat("_StepSize", Mathf.Lerp(1.5f, 2f, t));
+        mistMaterial.SetFloat("_MaxDistance", Mathf.Lerp(60f, 100f, t));
+        mistMaterial.SetFloat("_LightScattering", Mathf.Lerp(0.15f, 1f, t));
+    }
+
+    public void ResetMistSettings()
+    {
+        mistMaterial.SetFloat("_DensityMultiplier", 0.15f);
+        mistMaterial.SetFloat("_DensityThreshold", 0.276f);
+        mistMaterial.SetFloat("_NoiseTiling", 0.5f);
+        mistMaterial.SetFloat("_StepSize", 1.5f);
+        mistMaterial.SetFloat("_MaxDistance", 60f);
+        mistMaterial.SetFloat("_LightScattering", 0.175f);
     }
 }
